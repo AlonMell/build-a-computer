ec9ad0f540ba84c3b3fc021e8931c89a7e16bbe4
AlonMell <danil.g020@mail.ru>
2025-01-13 15:07:48 +0300

    refactor: props and func declaration

--- project6/hack_assembler.py
+++ project6/hack_assembler.py
@@ -4,24 +4,14 @@ from typing import TextIO
 
 class HackAssembler:
     def __init__(self):
-        self.__symbolTable = {
-            "R0":  0,  "R1":   1,  "R2":  2,  "R3":  3,
-            "R4":  4,  "R5":   5,  "R6":  6,  "R7":  7,
-            "R8":  8,  "R9":   9,  "R10": 10, "R11": 11,
-            "R12": 12, "R13":  13, "R14": 14, "R15": 15,
-            "SCREEN": 16384, "KBD": 24576,
-            "SP":  0,  "LCL":  1,  "ARG": 2,
-            "THIS": 3, "THAT": 4
-        }
+        self.__MACHINE_WORD = 16
+        self.__START_VAR_ADDR = 16
 
-        self.__dest = {
-            "null": "000",
-            "M": "001", "D":  "010", "DM": "011",
-            "A": "100", "AM": "101", "AD": "110",
-            "ADM": "111"
-        }
+        self.__A = "@"
+        self.__LABEL = "("
+        self.__COMMENT = "//"
 
-        self.__comp = {
+        self.__COMP = {
             "0":   "0101010",
             "1":   "0111111",
             "-1":  "0111010",
@@ -42,55 +32,66 @@ class HackAssembler:
             "D|A": "0010101", "D|M": "1010101"
         }
 
-        self.__jump = {
+        self.__DEST = {
             "null": "000",
-            "JGT": "001", "JEQ": "010", "JGE": "011",
-            "JLT": "100", "JNE": "101", "JLE": "110",
-            "JMP": "111"
+            "M":    "001", "D":  "010", "DM": "011",
+            "A":    "100", "AM": "101", "AD": "110",
+            "ADM":  "111"
         }
 
-        self.__cTable = {
-            "dest": self.__dest,
-            "comp": self.__comp,
-            "jump": self.__jump,
+        self.__JUMP = {
+            "null": "000",
+            "JGT":  "001", "JEQ": "010", "JGE": "011",
+            "JLT":  "100", "JNE": "101", "JLE": "110",
+            "JMP":  "111"
         }
 
-        self.__MACHINE_WORD = 16
-        self.__START_VAR_ADDR = 16
+        self.__C_TABLE = {
+            "dest": self.__DEST,
+            "comp": self.__COMP,
+            "jump": self.__JUMP,
+        }
 
-        self.__A = "@"
-        self.__LABEL = "("
-        self.__COMMENT = "//"
+        self.__symbol_table = {
+            "R0":  0,  "R1":   1,  "R2":  2,  "R3":  3,
+            "R4":  4,  "R5":   5,  "R6":  6,  "R7":  7,
+            "R8":  8,  "R9":   9,  "R10": 10, "R11": 11,
+            "R12": 12, "R13":  13, "R14": 14, "R15": 15,
+            "SCREEN": 16384, "KBD": 24576,
+            "SP":  0,  "LCL":  1,  "ARG": 2,
+            "THIS": 3, "THAT": 4
+        }
 
     def __is_starts_with(self, line: str, symb: str) -> bool:
         return line.strip().startswith(symb)
 
     def __skip_line(self, line: str) -> bool:
-        return (not line.strip()
-                or self.__is_starts_with(line, self.__COMMENT))
+        return (
+            not line.strip()
+            or self.__is_starts_with(line, self.__COMMENT)
+        )
 
     def __clean_line(self, line: str) -> str:
         return line.split(self.__COMMENT, 1)[0].strip()
 
     def __handle_label(self, line: str, line_number: int) -> None:
         label = line[1:-1]
-        if label not in self.__symbolTable:
-            self.__symbolTable[label] = line_number
+        if label not in self.__symbol_table:
+            self.__symbol_table[label] = line_number
 
     def __preprocess(self, asm_file: TextIO) -> list[str]:
         line_number = 0
         inter_code = []
 
         for line in asm_file:
-            if self.__skip_line(line):
-                continue
-            elif self.__is_starts_with(line, self.__LABEL):
-                self.__handle_label(self.__clean_line(line), line_number)
-                continue
-            else:
+            if self.__is_starts_with(line, self.__LABEL):
+                self.__handle_label(
+                    self.__clean_line(line),
+                    line_number
+                )
+            elif not self.__skip_line(line):
                 inter_code.append(self.__clean_line(line))
-
-            line_number += 1
+                line_number += 1
 
         return inter_code
 
@@ -102,42 +103,50 @@ class HackAssembler:
         a_instr = line[1:]
 
         if a_instr.isdigit():
-            return bin(int(a_instr))[2:].zfill(self.__MACHINE_WORD)
-        elif a_instr not in self.__symbolTable:
-            self.__symbolTable[a_instr] = var_addr[0]
+            return bin(
+                int(a_instr)
+            )[2:].zfill(self.__MACHINE_WORD)
+        elif a_instr not in self.__symbol_table:
+            self.__symbol_table[a_instr] = var_addr[0]
             var_addr[0] += 1
 
-        return bin(self.__symbolTable[a_instr])[2:].zfill(self.__MACHINE_WORD)
+        return bin(
+            self.__symbol_table[a_instr]
+        )[2:].zfill(self.__MACHINE_WORD)
 
     def __parse(self, line: str) -> str:
         dest_idx, jump_idx = line.find("="), line.find(";")
         is_dest_find, is_jump_find = dest_idx != -1, jump_idx != -1
 
-        dest = self.__cTable["dest"]["null"]
-        jump = self.__cTable["jump"]["null"]
+        dest = self.__C_TABLE["dest"]["null"]
+        jump = self.__C_TABLE["jump"]["null"]
         comp = ""
 
         if not is_dest_find and not is_jump_find:
             raise Exception("Compile error!")
 
         if is_dest_find:
-            dest = self.__cTable["dest"][line[:dest_idx]]
+            dest = self.__C_TABLE["dest"][line[:dest_idx]]
         if is_jump_find:
-            jump = self.__cTable["jump"][line[jump_idx+1:]]
+            jump = self.__C_TABLE["jump"][line[jump_idx+1:]]
 
         if is_dest_find and is_jump_find:
-            comp = self.__cTable["comp"][line[dest_idx+1:jump_idx]]
+            comp = self.__C_TABLE["comp"][line[dest_idx+1:jump_idx]]
         elif not is_dest_find and is_jump_find:
-            comp = self.__cTable["comp"][line[:jump_idx]]
+            comp = self.__C_TABLE["comp"][line[:jump_idx]]
         else:
-            comp = self.__cTable["comp"][line[dest_idx+1:]]
+            comp = self.__C_TABLE["comp"][line[dest_idx+1:]]
 
         return f"111{comp}{dest}{jump}"
 
     def __handle_C_instruction(self, line: str) -> str:
         return self.__parse(line)
 
-    def __compile(self, inter_code: list[str], bin_file: TextIO) -> None:
+    def __compile(
+        self,
+        inter_code: list[str],
+        bin_file: TextIO
+    ) -> None:
         var_addr = [self.__START_VAR_ADDR]
 
         for instr in inter_code:
@@ -150,9 +159,14 @@ class HackAssembler:
 
             print(res, file=bin_file)
 
-    def Compile(self, in_file_path: str, out_file_path: str = "") -> None:
+    def Compile(
+        self,
+        in_file_path: str,
+        out_file_path: str = ""
+    ) -> None:
         if out_file_path == "":
-            file_base_name, _ = os.path.splitext(os.path.basename(in_file_path))
+            file_name = os.path.basename(in_file_path)
+            file_base_name, _ = os.path.splitext(file_name)
             out_file_path = f"./{file_base_name}.hack"
 
         inter_code = []
@@ -162,9 +176,22 @@ class HackAssembler:
             self.__compile(inter_code, bin_file)
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Hack Assembler for .asm files")
-    parser.add_argument("inFile", type=str, help="Path to the input .asm file")
-    parser.add_argument("-o", "--outFile", type=str, help="Path to the output .hack file", default="")
+    parser = argparse.ArgumentParser(
+        description="Hack Assembler for .asm files"
+    )
+    parser.add_argument(
+        "inFile",
+        type=str,
+        help="Path to the input .asm file"
+    )
+    parser.add_argument(
+        "-o",
+        "--outFile",
+        type=str,
+        help="Path to the output .hack file",
+        default=""
+    )
+
     return parser.parse_args()
 
 def main():
